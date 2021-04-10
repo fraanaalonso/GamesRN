a7a6239d7e235e0d4f7b8e8978ca2b6f8a9a7ba0
Fran Alonso <fraloal97@gmail.com>
2021-04-10 21:07:57 +0200

    Initial Structure

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  )
+}
+
+
+export default App;
