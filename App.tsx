70c90114a312d820c3e3d64ebc55de44882ed3e4
Fran Alonso <fraloal97@gmail.com>
2021-04-10 22:52:03 +0200

    Ionicons config

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Navigator } from './src/navigation/Navigator';
+import { Tabs } from './src/navigation/Navigator';
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator />
+      <Tabs />
     </NavigationContainer>
   )
 }
